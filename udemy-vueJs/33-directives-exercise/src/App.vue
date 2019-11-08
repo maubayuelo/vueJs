67233f8bd4bee0d50589e3f897004f05cc65bf5b
Mauricio Bayuelo <mauricio@EXS-Programmer3.local>
2019-11-08 16:48:15 -0500

    adding directives exersice

---
+++ udemy-vueJs/33-directives-exercise/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container mt-5">
+        <form>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6" v-if="!submitted">
+                     <h1>Directives Exercise</h1>
+                      <!-- Exercise -->
+                      <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+                      <button v-customOn:click="clicked" class="btn btn-primary">Click Me</button>
+                      <hr>
+                      <div
+                              style="width: 100px; height: 100px; background-color: lightgreen"
+                              v-customOn:mouseenter="mouseEnter"
+                              v-customOn:mouseleave="mouseLeave"></div>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+
+<script>
+export default {
+directives: {
+    customOn: {
+        bind(el, binding) {
+//                    el.onclick = function() {
+//                        binding.value();
+//                    }
+            const type = binding.arg;
+            const fn = binding.value;
+            el.addEventListener(type, fn);
+        }
+    }
+},
+methods: {
+    clicked() {
+        alert('I was clicked!');
+    },
+    mouseEnter() {
+        console.log('Mouse entered!');
+    },
+    mouseLeave() {
+        console.log('Mouse leaved!');
+    }
+}
+}
+</script>
+
+<style>
+</style>
